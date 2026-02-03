96b8f3e7f11b878215ec4edade8784c6b9c42f35
Manasvee-9578 <manasvee9578@gmail.com>
2026-02-03 19:22:12 +0530

    Chapter 4: Operators

---
+++ Cha_4_Operators/Examples.py
@@ -0,0 +1,25 @@
+import math as m
+r1 = 2.5
+A = m.pi ** r1 
+print("Area of circul is:",A)
+print()
+
+a, b = 7, 3
+print(a + b, a - b, a * b, a / b, a // b, a % b, a ** b)
+print()
+
+x = 5
+x += 2  
+print(x) 
+print()
+
+print((5 < 2) or (3 < 4)) 
+print()
+
+print(not (5 == 2))   
+print()
+
+print(m.ceil(2.3), m.floor(2.7))  
+print()
+
+print(m.log(100, 10))     
---
+++ Cha_4_Operators/Mathematical_Fun.py
@@ -0,0 +1,67 @@
+import math
+
+x = math.sqrt(16)
+print(x)
+
+y = math.factorial(5)
+print(y)
+
+z = math.log(100, 2)
+print(z)
+
+a = math.ceil(4.3) # give next highest integer
+print(a)
+
+b = math.floor(4.7)# give next lowest integer
+print(b)
+
+c = math.degrees(3.14159)# convert radians to degrees
+print(c)
+
+d = math.radians(180)# convert degrees to radians
+print(d)
+
+e = math.sin(0.5)# sine of an angle in radians
+print(e)
+
+f = math.cos(0.5)# cosine of an angle in radians
+print(f)
+
+g = math.tan(0.5)# tangent of an angle in radians
+print(g)
+
+h = math.exp(0.4)# e raised to the power of 2
+print(h)
+
+i = math.pow(2, 3)# 2 raised to the power of 3
+print(i)
+
+j = math.fabs(-10)# give positive value 
+print(j)
+
+k = math.fmod(10, 3)# modulus
+print(k)    
+
+l = math.fsum([1.5,2.4,-3.3])# precise floating point sum
+print(l)
+
+m = math.fmod(14.5,3)# return floting and integer parts
+print(m)
+
+n = math.log10(2)# log base 10
+print(n)
+
+o = math.log(2, 2) # log of 2 with base 2
+print(o)
+
+p = math.gcd(25,30)# return greatest common divisor
+print(p)
+
+q = math.trunc(15.67657)# output:15
+print(q)
+
+r = math.isinf(15.2545)
+print(r)
+
+s = math.isnan(78)
+print(s)
