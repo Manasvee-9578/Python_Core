0678672ebacc9e4e61707126fa380eaebae5361a
Manasvee-9578 <manasvee9578@gmail.com>
2026-02-04 20:52:26 +0530

    Update data types examples

--- Cha_3_Datatypes/list_tup_set_dict.py
+++ Cha_3_Datatypes/list_tup_set_dict.py
@@ -1,55 +1,55 @@
-# #list example
-# list = [10,-20,2.5,"python",True]
-# print(list)
-# print(list[0])
-# print(list[1:3])
-# print(list.reverse())
-# print(list.__len__())
-# list[0] = 143
-# print(list)
+#list example
+list = [10,-20,2.5,"python",True]
+print(list)
+print(list[0])
+print(list[1:3])
+print(list.reverse())
+print(list.__len__())
+list[0] = 143
+print(list)
 
-# #tuple example
-# tuple = (10,-20,2.5,"python",True)
-# print(tuple)
-# print(tuple[0])
-# print(tuple[1:3])
-# print(tuple.__len__())
-# print("list can be modified but tuple cannot be modified")
+#tuple example
+tuple = (10,-20,2.5,"python",True)
+print(tuple)
+print(tuple[0])
+print(tuple[1:3])
+print(tuple.__len__())
+print("list can be modified but tuple cannot be modified")
 
 
-# #set example
-# set = {10,-20,2.5,"python",True}
-# print(set)
+#set example
+set = {10,-20,2.5,"python",True}
+print(set)
 
-# ch = set("hello")
-# print(ch)
+ch = set("hello")
+print(ch)
 
-# lst = [1,2,3,4,5,5,4,3,2,1]
-# print(set(lst))  #removes duplicates and converts into set
+lst = [1,2,3,4,5,5,4,3,2,1]
+print(set(lst))  #removes duplicates and converts into set
 
-# print(set[0:2])
-# set.update([100,200,300])
-# print(set)
-# set.remove(2.5)
-# print(set)
+print(set[0:2])
+set.update([100,200,300])
+print(set)
+set.remove(2.5)
+print(set)
 
-# #frozenset example
-# s = {1,2,3,4,5}
-# fs = frozenset(s)
-# print(fs)
+#frozenset example
+s = {1,2,3,4,5}
+fs = frozenset(s)
+print(fs)
 
-# #dictionary example
-# dict = {1:"python",2:"java",3:"c++",4:"c#",5:"javascript"}
-# print(dict)
-# print(dict.keys())
-# print(dict.values())
-# del dict[3]
-# print(dict)
+#dictionary example
+dict = {1:"python",2:"java",3:"c++",4:"c#",5:"javascript"}
+print(dict)
+print(dict.keys())
+print(dict.values())
+del dict[3]
+print(dict)
 
-# d ={}
-# d[10]="hello"
-# d[11]="world"
-# print(d)
+d ={}
+d[10]="hello"
+d[11]="world"
+print(d)
 
 print("hello \nworld" )
 print()
